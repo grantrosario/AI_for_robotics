d5eacbc2f5f6f4bca7b241cca51693648302cadd
Grant Rosario <grantster1124@aol.com>
2017-02-04 23:50:40 -0500

    Modified move function to be inexact

--- L1/AI_for_robotics-L1.py
+++ L1/AI_for_robotics-L1.py
@@ -5,6 +5,9 @@ world=['green', 'red', 'red', 'green', 'green']
 measurements = ['red', 'green']
 pHit = 0.6
 pMiss = 0.2
+pExact = 0.8
+pOvershoot = 0.1
+pUndershoot = 0.1
 
 def sense(p, Z):
     q = []
@@ -19,7 +22,10 @@ def sense(p, Z):
 def move(p, U):
 	q = []
 	for i in range(len(p)):
-		q.append(p[(i-U) % len(p)])
+		s = pExact * p[(i - U) % len(p)]
+		s = s + pOvershoot * p[(i-U-1) % len(p)]
+		s = s + pUndershoot * p[(i-U+1) % len(p)]
+		q.append(s)
 	return q
 
 # for k in range(len(measurements)):
