3d4e23a085f6e64439554b4bc0b1c049e2271990
Grant Rosario <grantster1124@aol.com>
2017-02-11 01:57:11 -0500

    Added KF predict function

--- L2 - KalmanFilters/AI_for_robotics-L2.py
+++ L2 - KalmanFilters/AI_for_robotics-L2.py
@@ -1,6 +1,12 @@
+
 def update(mean1, var1, mean2, var2):
     new_mean = (var2 * mean1 + var1 * mean2) / (var1 + var2)
-    new_var = 1 / ((1/var2) + (1/var1))
+    new_var = 1/(1/var1 + 1/var2)
     return [new_mean, new_var]
 
-print update(10.,8.,13., 2.)
+def predict(mean1, var1, mean2, var2):
+    new_mean = mean1 + mean2
+    new_var = var1 + var2
+    return [new_mean, new_var]
+
+print predict(10., 4., 12., 4.)
